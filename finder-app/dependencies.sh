3c1a2da77b06d4eb1686110899f7abe6c03de1e0
Ali-Bolucu <ali60blc24@gmail.com>
2024-11-07 21:15:15 +0300

    [Assignment3 Part2] Manual Linux Overview and Demo

--- finder-app/dependencies.sh
+++ finder-app/dependencies.sh
@@ -6,3 +6,21 @@
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y u-boot-tools
 sudo apt-get install -y qemu
+
+sudo apt-get install -y flex
+sudo apt-get install -y libelf-dev
+sudo apt-get install -y libudev-dev
+sudo apt-get install -y libpci-dev
+sudo apt-get install -y libiberty-dev
+sudo apt-get install -y libncurses-dev 
+sudo apt-get install -y bison
+sudo apt-get install -y openssl
+sudo apt-get install -y dkms
+sudo apt-get install -y autoconf
+
+
+sudo apt update
+sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+sudo adduser $USER libvirt
+sudo adduser $USER kvm
+
